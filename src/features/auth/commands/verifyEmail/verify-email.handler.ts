b5f8755b9db3da2a454f2a201ee98e0a316de98a
hoaturi <oink2716@gmail.com>
2024-12-09 17:15:39 +0900

    feat(auth): cleanup unused tokens after email verification

--- src/features/auth/commands/verifyEmail/verify-email.handler.ts
+++ src/features/auth/commands/verifyEmail/verify-email.handler.ts
@@ -4,10 +4,13 @@ import { EntityManager } from '@mikro-orm/postgresql';
 import { AuthError } from '../../auth.error';
 import { Result } from '../../../../common/result/result';
 import { ResultError } from '../../../../common/result/result-error';
-import { EmailVerification } from '../../../../domain/auth/email-verification.entity';
+import { EmailVerificationToken } from '../../../../domain/auth/email-verification-token.entity';
+import { Logger } from '@nestjs/common';
 
 @CommandHandler(VerifyEmailCommand)
 export class VerifyEmailHandler implements ICommandHandler<VerifyEmailCommand> {
+  private readonly logger = new Logger(VerifyEmailHandler.name);
+
   constructor(private readonly em: EntityManager) {}
 
   async execute(
@@ -16,18 +19,22 @@ export class VerifyEmailHandler implements ICommandHandler<VerifyEmailCommand> {
     const { token } = command.dto;
 
     const verification = await this.em.findOne(
-      EmailVerification,
+      EmailVerificationToken,
       {
         token,
         usedAt: null,
         expiresAt: { $gt: new Date() },
       },
       {
-        fields: ['usedAt', 'user.isVerified'],
+        fields: ['usedAt', 'user.isVerified', 'user.id'],
       },
     );
 
     if (!verification || verification.user.isVerified) {
+      this.logger.log(
+        { token, userId: verification?.user.id },
+        'auth.verify-email.failed: Invalid or expired token',
+      );
       return Result.failure(AuthError.InvalidToken);
     }
 
@@ -35,6 +42,17 @@ export class VerifyEmailHandler implements ICommandHandler<VerifyEmailCommand> {
     verification.user.isVerified = true;
 
     await this.em.flush();
+
+    await this.em.nativeDelete(EmailVerificationToken, {
+      id: { $ne: verification.id },
+      user: verification.user.id,
+    });
+
+    this.logger.log(
+      { userId: verification.user.id },
+      'auth.verify-email.success: Email verified',
+    );
+
     return Result.success();
   }
 }
--- test/auth/verify-email.handler.spec.ts
+++ test/auth/verify-email.handler.spec.ts
@@ -1,19 +1,22 @@
 import { Test } from '@nestjs/testing';
 import { EntityManager } from '@mikro-orm/postgresql';
-import { VerifyEmailHandler } from '../../src/features/auth/commands/verifyEmail/verify-email.handler';
+import {
+  VerifyEmailCommand,
+  VerifyEmailHandler,
+} from '../../src/features/auth/commands';
 import { User } from '../../src/domain/user/user.entity';
-import { EmailVerification } from '../../src/domain/auth/email-verification.entity';
-import { VerifyEmailCommand } from '../../src/features/auth/commands/verifyEmail/verify-email.command';
 import { AuthError } from '../../src/features/auth/auth.error';
+import { EmailVerificationToken } from '../../src/domain/auth/email-verification-token.entity';
 
 describe('VerifyEmailHandler', () => {
   let handler: VerifyEmailHandler;
-  let mockEntityManager: jest.Mocked<EntityManager>;
+  let em: jest.Mocked<EntityManager>;
 
   beforeEach(async () => {
-    mockEntityManager = {
+    em = {
       findOne: jest.fn(),
       flush: jest.fn(),
+      nativeDelete: jest.fn(),
     } as any;
 
     const moduleRef = await Test.createTestingModule({
@@ -21,7 +24,7 @@ describe('VerifyEmailHandler', () => {
         VerifyEmailHandler,
         {
           provide: EntityManager,
-          useValue: mockEntityManager,
+          useValue: em,
         },
       ],
     }).compile();
@@ -31,18 +34,20 @@ describe('VerifyEmailHandler', () => {
 
   it('should successfully verify email when token is valid', async () => {
     // Arrange
-    const user = {
+    const mockUser = {
+      id: 'user-id',
       isVerified: false,
-    } as User;
+    };
 
-    const verification = {
+    const mockVerification = {
+      id: 'token-id',
       token: 'valid-token',
       usedAt: null,
-      expiresAt: new Date(Date.now() + 60 * 30 * 1000),
-      user,
-    } as EmailVerification;
+      expiresAt: new Date(Date.now() + 1000),
+      user: mockUser,
+    };
 
-    mockEntityManager.findOne.mockResolvedValue(verification);
+    em.findOne.mockResolvedValue(mockVerification);
 
     // Act
     const result = await handler.execute(
@@ -51,50 +56,78 @@ describe('VerifyEmailHandler', () => {
 
     // Assert
     expect(result.isSuccess).toBe(true);
-    expect(mockEntityManager.findOne).toHaveBeenCalled();
-    expect(verification.usedAt).toBeDefined();
-    expect(verification.user.isVerified).toBe(true);
-    expect(mockEntityManager.flush).toHaveBeenCalled();
-
-    it('should fail when token is invalid', async () => {
-      // Arrange
-      mockEntityManager.findOne.mockResolvedValue(null);
-
-      // Act
-      const result = await handler.execute(
-        new VerifyEmailCommand({ token: 'invalid-token' }),
-      );
-
-      // Assert
-      expect(result.isSuccess).toBe(false);
-      expect(result.error).toBe(AuthError.InvalidToken);
-      expect(mockEntityManager.flush).not.toHaveBeenCalled();
+    expect(mockVerification.usedAt).toBeDefined();
+    expect(mockUser.isVerified).toBe(true);
+    expect(em.flush).toHaveBeenCalled();
+    expect(em.nativeDelete).toHaveBeenCalledWith(EmailVerificationToken, {
+      id: { $ne: mockVerification.id },
+      user: mockUser.id,
     });
+  });
 
-    it('should fail when user is already verified', async () => {
-      // Arrange
-      const user = {
-        isVerified: true,
-      } as User;
+  it('should return failure when token is not found', async () => {
+    // Arrange
+    em.findOne.mockResolvedValue(null);
 
-      const verification = {
-        token: 'valid-token',
-        usedAt: null,
-        expiresAt: new Date(Date.now() + 60 * 30 * 1000),
-        user,
-      } as EmailVerification;
+    // Act
+    const result = await handler.execute(
+      new VerifyEmailCommand({ token: 'invalid-token' }),
+    );
 
-      mockEntityManager.findOne.mockResolvedValue(verification);
+    // Assert
+    expect(result.isSuccess).toBe(false);
+    expect(result.error).toBe(AuthError.InvalidToken);
+    expect(em.flush).not.toHaveBeenCalled();
+    expect(em.nativeDelete).not.toHaveBeenCalled();
+  });
 
-      // Act
-      const result = await handler.execute(
-        new VerifyEmailCommand({ token: 'valid-token' }),
-      );
+  it('should return failure when token is expired', async () => {
+    // Arrange
+    jest.useFakeTimers();
+    const now = new Date('2024-01-01T00:00:00Z');
+    jest.setSystemTime(now);
 
-      // Assert
-      expect(result.isSuccess).toBe(false);
-      expect(result.error).toBe(AuthError.InvalidToken);
-      expect(mockEntityManager.flush).not.toHaveBeenCalled();
-    });
+    em.findOne.mockResolvedValue(null);
+
+    // Act
+    const result = await handler.execute(
+      new VerifyEmailCommand({ token: 'expired-token' }),
+    );
+
+    // Assert
+    expect(result.isSuccess).toBe(false);
+    expect(result.error).toBe(AuthError.InvalidToken);
+    expect(em.flush).not.toHaveBeenCalled();
+    expect(em.nativeDelete).not.toHaveBeenCalled();
+
+    jest.useRealTimers();
+  });
+
+  it('should return failure when user is already verified', async () => {
+    // Arrange
+    const mockUser = {
+      id: 'user-id',
+      isVerified: true,
+    } as User;
+
+    const mockVerification = {
+      token: 'valid-token',
+      usedAt: null,
+      expiresAt: new Date(Date.now() + 1000),
+      user: mockUser,
+    } as EmailVerificationToken;
+
+    em.findOne.mockResolvedValue(mockVerification);
+
+    // Act
+    const result = await handler.execute(
+      new VerifyEmailCommand({ token: 'valid-token' }),
+    );
+
+    // Assert
+    expect(result.isSuccess).toBe(false);
+    expect(result.error).toBe(AuthError.InvalidToken);
+    expect(em.flush).not.toHaveBeenCalled();
+    expect(em.nativeDelete).not.toHaveBeenCalled();
   });
 });
