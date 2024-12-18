156c1b89cf69848ffa384dc40401507ef896b8df
hoaturi <oink2716@gmail.com>
2024-12-19 03:37:25 +0900

    feat(seeder): add job preference data seeder

---
+++ src/database/seeds/base.seeder.ts
@@ -0,0 +1,37 @@
+import { Seeder } from '@mikro-orm/seeder';
+import { EntityManager } from '@mikro-orm/postgresql';
+import { JobCategory } from '../../domain/common/entities/job-category.entity';
+import { EmploymentType } from '../../domain/common/entities/employment-type.entity';
+import { Language } from '../../domain/common/entities/language.entity';
+import { WorkLocationType } from '../../domain/common/entities/work-location-type.entity';
+import {
+  EMPLOYMENT_TYPES,
+  JOB_CATEGORIES,
+  LANGUAGES,
+  WORK_LOCATION_TYPES,
+} from '../../domain/common/constants';
+
+export class BaseSeeder extends Seeder {
+  public async run(em: EntityManager): Promise<void> {
+    JOB_CATEGORIES.map((category) =>
+      em.create(JobCategory, new JobCategory(category.name, category.slug)),
+    );
+
+    EMPLOYMENT_TYPES.map((type) =>
+      em.create(EmploymentType, new EmploymentType(type.name, type.slug)),
+    );
+
+    LANGUAGES.map((language) =>
+      em.create(Language, new Language(language.name, language.slug)),
+    );
+
+    WORK_LOCATION_TYPES.map((location) =>
+      em.create(
+        WorkLocationType,
+        new WorkLocationType(location.name, location.slug),
+      ),
+    );
+
+    await em.flush();
+  }
+}
