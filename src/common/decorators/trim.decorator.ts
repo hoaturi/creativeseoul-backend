85cd825f7abf15d2a487ee0845c42125aacae557
hoaturi <oink2716@gmail.com>
2024-12-23 21:37:24 +0900

    feat(decorator): add tag validation and trim decorators

---
+++ src/common/decorators/trim.decorator.ts
@@ -0,0 +1,13 @@
+import { Transform } from 'class-transformer';
+
+export function Trim({ each = false }: { each?: boolean } = {}) {
+  return Transform((params) => {
+    const value = params.value;
+
+    if (each) {
+      return value.map((v: string) => v.trim());
+    }
+
+    return value.trim();
+  });
+}
