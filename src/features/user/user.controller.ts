c44fc12db940b5690e65991c33f0d0ea5c0590c2
hoaturi <oink2716@gmail.com>
2024-12-25 02:46:19 +0900

    chore(auth): remove unused getCurrentUser and changeUsername endpoints

--- src/features/user/user.controller.ts
+++ src/features/user/user.controller.ts
@@ -1,14 +1,13 @@
 import {
   Body,
   Controller,
-  Get,
   HttpException,
   Patch,
   Req,
   Res,
   UseGuards,
 } from '@nestjs/common';
-import { CommandBus, QueryBus } from '@nestjs/cqrs';
+import { CommandBus } from '@nestjs/cqrs';
 import { ChangePasswordRequestDto } from './dtos/change-password-request.dto';
 import { ChangePasswordCommand } from './commands/change-password/change-password.command';
 import { CurrentUser } from '../../infrastructure/security/decorators/current-user.decorator';
@@ -22,23 +21,18 @@ import { UserError } from './user.error';
 import { AuthGuard } from '../../infrastructure/security/guards/auth.guard';
 import { AuthError } from '../auth/auth.error';
 import { CommonError } from '../common/common.error';
-import { ChangeUsernameRequestDto } from './dtos/change-username-request.dto';
-import { ChangeUsernameCommand } from './commands/change-username/change-username.command';
-import { GetCurrentUserResponseDto } from './dtos/get-current-user-response.dto';
-import { GetCurrentUserQuery } from './query/get-current-user/get-current-user.query';
 import { Request, Response } from 'express';
 
 @UseGuards(AuthGuard)
 @ApiUnauthorizedResponse({ example: AuthError.Unauthenticated })
 @Controller('users')
 export class UserController {
-  public constructor(
-    private readonly commandBus: CommandBus,
-    private readonly queryBUs: QueryBus,
-  ) {}
+  public constructor(private readonly commandBus: CommandBus) {}
 
-  @Patch('change-password')
+  @Patch('password')
+  @UseGuards(AuthGuard)
   @ApiOkResponse()
+  @ApiUnauthorizedResponse({ example: AuthError.Unauthenticated })
   @ApiBadRequestResponse({
     examples: {
       ValidationFailed: {
@@ -77,49 +71,6 @@ export class UserController {
         resolve();
       });
     });
-
-    return result.value;
-  }
-
-  @Patch('change-username')
-  @ApiOkResponse()
-  @ApiBadRequestResponse({
-    examples: {
-      ValidationFailed: {
-        summary: 'Validation failed',
-        value: CommonError.ValidationFailed,
-      },
-    },
-  })
-  public async changeUsername(
-    @CurrentUser() user: AuthenticatedUser,
-    @Body() dto: ChangeUsernameRequestDto,
-  ): Promise<void> {
-    const result = await this.commandBus.execute(
-      new ChangeUsernameCommand(user.id, dto.username),
-    );
-
-    if (!result.isSuccess) {
-      throw new HttpException(result.error, result.error.statusCode);
-    }
-
-    return result.value;
-  }
-
-  @Get('current')
-  @ApiOkResponse({
-    type: GetCurrentUserResponseDto,
-  })
-  public async getCurrentUser(
-    @CurrentUser() user: AuthenticatedUser,
-  ): Promise<GetCurrentUserResponseDto> {
-    const result = await this.queryBUs.execute(
-      new GetCurrentUserQuery(user.id),
-    );
-
-    if (!result.isSuccess) {
-      throw new HttpException(result.error, result.error.statusCode);
-    }
 
     return result.value;
   }
--- src/features/user/user.module.ts
+++ src/features/user/user.module.ts
@@ -1,17 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { ChangePasswordHandler } from './commands/change-password/change-password.handler';
-import { ChangeUsernameHandler } from './commands/change-username/change-username.handler';
-import { GetCurrentUserHandler } from './query/get-current-user/get-current-user.handler';
-
-const handlers = [
-  ChangePasswordHandler,
-  ChangeUsernameHandler,
-  GetCurrentUserHandler,
-];
 
 @Module({
   controllers: [UserController],
-  providers: [...handlers],
+  providers: [ChangePasswordHandler],
 })
 export class UserModule {}
