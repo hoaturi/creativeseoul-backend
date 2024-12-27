b07d26def2c46956167d50251dbc054431fecdc0
hoaturi <oink2716@gmail.com>
2024-12-27 15:04:08 +0900

    chore(entity): remove unused entities and update seeder

--- src/database/seeds/base.seeder.ts
+++ src/database/seeds/base.seeder.ts
@@ -1,14 +1,7 @@
 import { Seeder } from '@mikro-orm/seeder';
 import { EntityManager } from '@mikro-orm/postgresql';
-import {
-  COUNTRIES,
-  EMPLOYMENT_TYPES,
-  LANGUAGES,
-  LOCATION_TYPES,
-} from '../../domain/common/constants';
-import { EmploymentType } from '../../domain/common/entities/employment-type.entity';
+import { COUNTRIES, LANGUAGES } from '../../domain/common/constants';
 import { Language } from '../../domain/common/entities/language.entity';
-import { WorkLocationType } from '../../domain/common/entities/work-location-type.entity';
 import { Country } from '../../domain/common/entities/country.entity';
 
 export class BaseSeeder extends Seeder {
@@ -30,19 +23,8 @@ export class BaseSeeder extends Seeder {
       console.log(`${entityName}: ${added} new records added`);
     }
 
-    await seedMissingEntities(
-      EmploymentType,
-      EMPLOYMENT_TYPES,
-      EmploymentType,
-      'Employment Types',
-    );
     await seedMissingEntities(Language, LANGUAGES, Language, 'Languages');
-    await seedMissingEntities(
-      WorkLocationType,
-      LOCATION_TYPES,
-      WorkLocationType,
-      'Work Location Types',
-    );
+
     await seedMissingEntities(Country, COUNTRIES, Country, 'Countries');
 
     await em.flush();
