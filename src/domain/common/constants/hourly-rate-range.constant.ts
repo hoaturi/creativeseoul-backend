666a40b0a01beed1f14d254d851342b12afcc3ca
hoaturi <oink2716@gmail.com>
2024-12-27 15:05:19 +0900

    feat(constant): add Ids to constants objects

--- src/domain/common/constants/hourly-rate-range.constant.ts
+++ src/domain/common/constants/hourly-rate-range.constant.ts
@@ -1,8 +1,8 @@
 export const HOURLY_RATE_RANGE = [
-  { name: '₩0 - ₩10K', slug: '0-10' },
-  { name: '₩10K - ₩20K', slug: '10-20' },
-  { name: '₩20K - ₩35K', slug: '20-35' },
-  { name: '₩35K - ₩50K', slug: '35-50' },
-  { name: '₩50K - ₩65K', slug: '50-65' },
-  { name: '₩65K+', slug: '65+' },
+  { id: 1, name: '₩0 - ₩10K', slug: '0-10' },
+  { id: 2, name: '₩10K - ₩20K', slug: '10-20' },
+  { id: 3, name: '₩20K - ₩35K', slug: '20-35' },
+  { id: 4, name: '₩35K - ₩50K', slug: '35-50' },
+  { id: 5, name: '₩50K - ₩65K', slug: '50-65' },
+  { id: 6, name: '₩65K+', slug: '65+' },
 ] as const;
--- src/domain/common/constants/salary-range.constant.ts
+++ src/domain/common/constants/salary-range.constant.ts
@@ -1,8 +1,8 @@
 export const SALARY_RANGE = [
-  { name: '₩0 - ₩25M', slug: '0-25' },
-  { name: '₩25M - ₩40M', slug: '25-40' },
-  { name: '₩40M - ₩60M', slug: '40-60' },
-  { name: '₩60M - ₩80M', slug: '60-80' },
-  { name: '₩80M - ₩100M', slug: '80-100' },
-  { name: '₩100M+', slug: '100+' },
+  { id: 1, name: '₩0 - ₩25M', slug: '0-25' },
+  { id: 2, name: '₩25M - ₩40M', slug: '25-40' },
+  { id: 3, name: '₩40M - ₩60M', slug: '40-60' },
+  { id: 4, name: '₩60M - ₩80M', slug: '60-80' },
+  { id: 5, name: '₩80M - ₩100M', slug: '80-100' },
+  { id: 6, name: '₩100M+', slug: '100+' },
 ] as const;
